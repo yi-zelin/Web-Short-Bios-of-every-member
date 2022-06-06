cda1452a0cb1cde80a914b1d756cf9107be144e9
yzl <fjt086@gmail.com>
2022-06-06 12:20:57 -0700

    1.0

--- IT100FinalProject/urls.py
+++ IT100FinalProject/urls.py
@@ -19,7 +19,7 @@ from app01 import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('mainpage/', views.mainpage),
+    path('index/', views.mainpage),
     path('user/list/', views.user_list),
 
 
